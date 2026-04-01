ba204982ecd10a148f5ba764d7413f2baf7f9adb
NNADuong-CS <duong342005@gmail.com>
2026-04-01 15:34:42 +0700

    .

--- src/backend/app/rag/memory/firebase_init.py
+++ src/backend/app/rag/memory/firebase_init.py
@@ -3,7 +3,7 @@ from firebase_admin import credentials, firestore
 
 # 🔥 tránh init nhiều lần
 if not firebase_admin._apps:
-    cred = credentials.Certificate("src/rag-knowledge-system-firebase-adminsdk-fbsvc-bb575ec243.json")
+    cred = credentials.Certificate("rag-knowledge-system-firebase-adminsdk-fbsvc-bb575ec243.json")
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
